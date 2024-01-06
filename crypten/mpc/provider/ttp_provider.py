2659a7e75a06ae1b9b650483146ea02008183655
leonardodalinky <linkyy2000313@gmail.com>
2024-01-06 17:15:55 +0800

    feat: async add & square triple

--- crypten/mpc/provider/ttp_provider.py
+++ crypten/mpc/provider/ttp_provider.py
@@ -6,17 +6,19 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+from abc import ABC
+from typing import Any
+
+import torch
+import torch.distributed as dist
 
 import crypten
 import crypten.communicator as comm
-import torch
-import torch.distributed as dist
 from crypten.common.rng import generate_kbit_random_tensor, generate_random_ring_element
 from crypten.common.util import count_wraps
 from crypten.mpc.primitives import ArithmeticSharedTensor, BinarySharedTensor
 
 from .provider import TupleProvider
-
 
 TTP_FUNCTIONS = ["additive", "square", "binary", "wraps", "B2A"]
 
@@ -32,9 +34,7 @@ class TrustedThirdParty(TupleProvider):
         b = generate_random_ring_element(size1, generator=generator, device=device)
         if comm.get().get_rank() == 0:
             # Request c from TTP
-            c = TTPClient.get().ttp_request(
-                "additive", device, size0, size1, op, *args, **kwargs
-            )
+            c = TTPClient.get().ttp_request("additive", device, size0, size1, op, *args, **kwargs)
         else:
             # TODO: Compute size without executing computation
             c_size = getattr(torch, op)(a, b, *args, **kwargs).size()
@@ -45,6 +45,10 @@ class TrustedThirdParty(TupleProvider):
         c = ArithmeticSharedTensor.from_shares(c, precision=0)
 
         return a, b, c
+
+    def async_generate_additive_triple(self, size0, size1, op, device=None, *args, **kwargs):
+        """Generate multiplicative triples of given sizes"""
+        return GenAddTripleTTPAction(size0, size1, op, device, *args, **kwargs)
 
     def square(self, size, device=None):
         """Generate square double of given size"""
@@ -60,6 +64,10 @@ class TrustedThirdParty(TupleProvider):
         r = ArithmeticSharedTensor.from_shares(r, precision=0)
         r2 = ArithmeticSharedTensor.from_shares(r2, precision=0)
         return r, r2
+
+    def async_square(self, size, device=None):
+        """Generate square double of given size"""
+        return SquareTTPAction(size, device)
 
     def generate_binary_triple(self, size0, size1, device=None):
         """Generate binary triples of given size"""
@@ -90,9 +98,7 @@ class TrustedThirdParty(TupleProvider):
             # Request theta_r from TTP
             theta_r = TTPClient.get().ttp_request("wraps", device, size)
         else:
-            theta_r = generate_random_ring_element(
-                size, generator=generator, device=device
-            )
+            theta_r = generate_random_ring_element(size, generator=generator, device=device)
 
         r = ArithmeticSharedTensor.from_shares(r, precision=0)
         theta_r = ArithmeticSharedTensor.from_shares(theta_r, precision=0)
@@ -103,9 +109,7 @@ class TrustedThirdParty(TupleProvider):
         generator = TTPClient.get().get_generator(device=device)
 
         # generate random bit
-        rB = generate_kbit_random_tensor(
-            size, bitlength=1, generator=generator, device=device
-        )
+        rB = generate_kbit_random_tensor(size, bitlength=1, generator=generator, device=device)
 
         if comm.get().get_rank() == 0:
             # Request rA from TTP
@@ -142,9 +146,7 @@ class TTPClient:
         def _setup_generators(self):
             """Setup RNG generator shared between each party (client) and the TTPServer"""
             seed = torch.empty(size=(), dtype=torch.long)
-            dist.irecv(
-                tensor=seed, src=comm.get().get_ttp_rank(), group=self.ttp_group
-            ).wait()
+            dist.irecv(tensor=seed, src=comm.get().get_ttp_rank(), group=self.ttp_group).wait()
             dist.barrier(group=self.ttp_group)
 
             self.generator = torch.Generator(device="cpu")
@@ -165,10 +167,8 @@ class TTPClient:
             else:
                 return self.generator
 
-        def ttp_request(self, func_name, device, *args, **kwargs):
-            assert (
-                comm.get().get_rank() == 0
-            ), "Only party 0 communicates with the TTPServer"
+        def ttp_request(self, func_name, device, *args, **kwargs) -> torch.LongTensor:
+            assert comm.get().get_rank() == 0, "Only party 0 communicates with the TTPServer"
             if device is not None:
                 device = str(device)
             message = {
@@ -187,6 +187,35 @@ class TTPClient:
 
             return result
 
+        def batched_ttp_request(
+            self, request_args_list: list[dict[str, Any]]
+        ) -> list[torch.LongTensor]:
+            assert comm.get().get_rank() == 0, "Only party 0 communicates with the TTPServer"
+            message = {
+                "function": "batched",
+                "device": None,
+                "args": request_args_list,
+                "kwargs": {},
+            }
+            ttp_rank = comm.get().get_ttp_rank()
+
+            comm.get().send_obj(message, ttp_rank, self.ttp_group)
+
+            sizes: list[int] = comm.get().recv_obj(ttp_rank, self.ttp_group)
+            assert len(sizes) == len(request_args_list)
+            result_buf = torch.empty(sum(sizes), dtype=torch.long, device="cpu")
+            comm.get().broadcast(result_buf, ttp_rank, self.comm_group)
+
+            #
+            results = []
+            for size, request_args in zip(sizes, request_args_list):
+                result = result_buf[:size]
+                result = result.to(device=request_args["device"])
+                result_buf = result_buf[size:]
+                results.append(result)
+
+            return results
+
     @staticmethod
     def _init():
         """Initializes a Trusted Third Party client that sends requests"""
@@ -200,7 +229,7 @@ class TTPClient:
         TTPClient.__instance = None
 
     @staticmethod
-    def get():
+    def get() -> "__TTPClient":
         """Returns the instance of the TTPClient"""
         if TTPClient.__instance is None:
             raise RuntimeError("TTPClient is not initialized")
@@ -231,18 +260,36 @@ class TTPServer:
                 if message == "terminate":
                     logging.info("TTPServer shutting down.")
                     return
+                elif message["function"] == "batched":
+                    # batchify
+                    results: list[torch.LongTensor] = []
+                    sizes: list[int] = []
+                    request_args_list: list[dict[str, Any]] = message["args"]
+                    for request_args in request_args_list:
+                        function = request_args["function"]
+                        device = request_args["device"]
+                        args = request_args["args"]
+                        kwargs = request_args["kwargs"]
+                        self.device = device
 
-                function = message["function"]
-                device = message["device"]
-                args = message["args"]
-                kwargs = message["kwargs"]
+                        result: torch.LongTensor = getattr(self, function)(*args, **kwargs)
+                        sizes.append(int(result.size()))
+                        results.append(result.to(device="cpu"))
+                    comm.get().send_obj(sizes, 0, self.ttp_group)
+                    comm.get().broadcast(torch.cat(results, dim=0), ttp_rank, self.comm_group)
+                else:
+                    # single instruction
+                    function = message["function"]
+                    device = message["device"]
+                    args = message["args"]
+                    kwargs = message["kwargs"]
 
-                self.device = device
+                    self.device = device
 
-                result = getattr(self, function)(*args, **kwargs)
+                    result = getattr(self, function)(*args, **kwargs)
 
-                comm.get().send_obj(result.size(), 0, self.ttp_group)
-                comm.get().broadcast(result, ttp_rank, self.comm_group)
+                    comm.get().send_obj(result.size(), 0, self.ttp_group)
+                    comm.get().broadcast(result, ttp_rank, self.comm_group)
         except RuntimeError as err:
             logging.info("Encountered Runtime error. TTPServer shutting down:")
             logging.info(f"{err}")
@@ -252,9 +299,7 @@ class TTPServer:
         ws = comm.get().get_world_size()
 
         seeds = [torch.randint(-(2**63), 2**63 - 1, size=()) for _ in range(ws)]
-        reqs = [
-            dist.isend(tensor=seeds[i], dst=i, group=self.ttp_group) for i in range(ws)
-        ]
+        reqs = [dist.isend(tensor=seeds[i], dst=i, group=self.ttp_group) for i in range(ws)]
         self.generators = [torch.Generator(device="cpu") for _ in range(ws)]
         self.generators_cuda = [
             (torch.Generator(device="cuda") if torch.cuda.is_available() else None)
@@ -309,7 +354,6 @@ class TTPServer:
         return result
 
     def additive(self, size0, size1, op, *args, **kwargs):
-
         # Add all shares of `a` and `b` to get plaintext `a` and `b`
         a = self._get_additive_PRSS(size0)
         b = self._get_additive_PRSS(size1)
@@ -350,3 +394,214 @@ class TTPServer:
         rA = rB - self._get_additive_PRSS(size, remove_rank=True)
 
         return rA
+
+
+#################
+#               #
+#    Actions    #
+#               #
+#################
+class TTPAction(ABC):
+    def pre_request_stage(self) -> Any:
+        """Prepares for the request"""
+        ...
+
+    def ttp_request_args(self) -> dict[str, Any] | None:
+        """Args of TTP requrests. If None, no communication is needed."""
+        ...
+
+    def post_request_stage(self, pre_stage_output: Any, ttp_request_result: torch.LongTensor):
+        """Post processing of the request"""
+        ...
+
+    def wait(self) -> None:
+        """Waits for the single request to complete"""
+        if self.is_completed():
+            return
+        # pre stage
+        pre_stage_output = self.pre_request_stage()
+        # merge ttp request
+        request_args = self.ttp_request_args()
+        if request_args is not None:
+            ttp_result = TTPClient.get().ttp_request(**request_args)
+        else:
+            ttp_result = None
+        # post stage
+        self.post_request_stage(pre_stage_output, ttp_result)
+
+    def is_completed(self) -> bool:
+        """Checks if the request is completed"""
+        ...
+
+    def get_result(self) -> Any:
+        """Returns the result of the request"""
+        ...
+
+    def group(self, *actions: "TTPAction") -> "TTPActionGroup":
+        """Groups this action with other actions"""
+        actions = (self, *actions)
+        return TTPActionGroup(self, *actions)
+
+
+class GenAddTripleTTPAction(TTPAction):
+    def __init__(
+        self,
+        size0: int,
+        size1: int,
+        op: str,
+        device: torch.device | str | None = None,
+        *args,
+        **kwargs,
+    ):
+        """Action for generating an additive triple.
+
+        Args:
+            size0 (int): size of the first operand
+            size1 (int): size of the second operand
+            op (str): operation to perform on the operands
+            device (torch.device | str | None): device to perform the operation on
+            *args: additional arguments
+            **kwargs: additional keyword arguments
+        """
+        self.size0 = size0
+        self.size1 = size1
+        self.op = op
+        self.device = device
+        self.args = args
+        self.kwargs = kwargs
+        self._result = None
+
+    def pre_request_stage(self) -> Any:
+        """Prepares for the request"""
+        generator = TTPClient.get().get_generator(device=self.device)
+
+        a = generate_random_ring_element(self.size0, generator=generator, device=self.device)
+        b = generate_random_ring_element(self.size1, generator=generator, device=self.device)
+
+        return a, b
+
+    def ttp_request_args(self) -> dict[str, Any] | None:
+        """Args of TTP requrests"""
+        if comm.get().get_rank() == 0:
+            return {
+                "func_name": "additive",
+                "device": str(self.device) if self.device is not None else None,
+                "args": (self.size0, self.size1, self.op, *self.args),
+                "kwargs": self.kwargs,
+            }
+        else:
+            return None
+
+    def post_request_stage(self, pre_stage_output: Any, ttp_request_result: torch.LongTensor):
+        """Post processing of the request"""
+        a, b = pre_stage_output
+        if comm.get().get_rank() == 0:
+            # Request c from TTP
+            c = ttp_request_result
+        else:
+            # TODO: Compute size without executing computation
+            generator = TTPClient.get().get_generator(device=self.device)
+            c_size = getattr(torch, self.op)(a, b, *self.args, **self.kwargs).size()
+            c = generate_random_ring_element(c_size, generator=generator, device=self.device)
+
+        a = ArithmeticSharedTensor.from_shares(a, precision=0)
+        b = ArithmeticSharedTensor.from_shares(b, precision=0)
+        c = ArithmeticSharedTensor.from_shares(c, precision=0)
+
+        self._result = (a, b, c)
+
+    def is_completed(self) -> bool:
+        """Checks if the request is completed"""
+        return self._result is not None
+
+    def get_result(self) -> Any:
+        """Returns the result of the request"""
+        return self._result
+
+
+class SquareTTPAction(TTPAction):
+    def __init__(self, size: int, device: torch.device | str | None = None):
+        """Action for generating a square double.
+
+        Args:
+            size (int): size of the operand
+            device (torch.device | str | None): device to perform the operation on
+        """
+        self.size = size
+        self.device = device
+        self._result = None
+
+    def pre_request_stage(self) -> Any:
+        """Prepares for the request"""
+        generator = TTPClient.get().get_generator(device=self.device)
+
+        r = generate_random_ring_element(self.size, generator=generator, device=self.device)
+        return r
+
+    def ttp_request_args(self) -> dict[str, Any] | None:
+        """Args of TTP requrests"""
+        if comm.get().get_rank() == 0:
+            return {
+                "func_name": "square",
+                "device": str(self.device) if self.device is not None else None,
+                "args": (self.size,),
+                "kwargs": {},
+            }
+        else:
+            return None
+
+    def post_request_stage(self, pre_stage_output: Any, ttp_request_result: torch.LongTensor):
+        """Post processing of the request"""
+        r = pre_stage_output
+        if comm.get().get_rank() == 0:
+            # Request r2 from TTP
+            r2 = ttp_request_result
+        else:
+            generator = TTPClient.get().get_generator(device=self.device)
+            r2 = generate_random_ring_element(self.size, generator=generator, device=self.device)
+
+        r = ArithmeticSharedTensor.from_shares(r, precision=0)
+        r2 = ArithmeticSharedTensor.from_shares(r2, precision=0)
+        self._result = (r, r2)
+
+    def is_completed(self) -> bool:
+        """Checks if the request is completed"""
+        return self._result is not None
+
+    def get_result(self) -> Any:
+        """Returns the result of the request"""
+        return self._result
+
+
+class TTPActionGroup:
+    def __init__(self, *actions: TTPAction) -> None:
+        self.actions = list(actions)
+
+    def wait(self) -> None:
+        """Waits for all actions to complete"""
+        if all(action.is_completed() for action in self.actions):
+            return
+        # pre stage
+        pre_stage_outputs = [action.pre_request_stage() for action in self.actions]
+        # merge ttp request
+        request_args_list = [action.ttp_request_args() for action in self.actions]
+        if all(request_args is not None for request_args in request_args_list):
+            ttp_results = TTPClient.get().batched_ttp_request(request_args_list)
+        else:
+            ttp_results = [None] * len(self.actions)
+        # post stage
+        for action, pre_stage_output, ttp_request_result in zip(
+            self.actions, pre_stage_outputs, ttp_results
+        ):
+            action.post_request_stage(pre_stage_output, ttp_request_result)
+
+    def is_completed(self) -> bool:
+        return all(action.is_completed() for action in self.actions)
+
+    def get_result(self) -> tuple[Any]:
+        return tuple(action.get_result() for action in self.actions)
+
+    def add(self, *actions: TTPAction) -> "TTPActionGroup":
+        assert all(isinstance(action, TTPAction) for action in actions)
+        self.actions.extend(actions)
+        return self
